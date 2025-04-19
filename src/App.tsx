674f88eb3fabbfe903302f13cc844cd2c56a31f0
Muzaffar Yusupov <153423021+muzaffaryusupov1@users.noreply.github.com>
2025-04-19 17:39:51 +0500

    first commit init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components'
+import { Button } from './components/ui/button'
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<Button>Click me</Button>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import HeaderTop from './header-top'
+
+const Header = () => {
+	return (
+		<div>
+			<HeaderTop />
+			Header
+		</div>
+	)
+}
+
+export default Header
